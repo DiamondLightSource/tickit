1e58d8c2bfaf845d34ae6bf628b2669f26506c9d
Callum Forrester <callum.forrester@diamond.ac.uk>
2021-06-10 19:55:40 +0100

    Remove template boilerplate

--- tickit/__init__.py
+++ tickit/__init__.py
@@ -1,4 +1,3 @@
 from tickit._version_git import __version__
-from tickit.tickit import HelloClass, say_hello_lots
 
-__all__ = ["__version__", "HelloClass", "say_hello_lots"]
+__all__ = ["__version__"]
