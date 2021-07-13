606286aeb071a1832cdef26321b53166446c1052
O'Donnell, Garry (DLSLtd,RAL,LSCI) <garry.o'donnell@diamond.ac.uk>
2021-07-13 12:56:34 +0000

    Refactor to fix pipenv entry point

---
+++ tickit/core/lifetime_runnable.py
@@ -0,0 +1,25 @@
+import asyncio
+import traceback
+from typing import Iterable, Protocol, runtime_checkable
+
+
+@runtime_checkable
+class LifetimeRunnable(Protocol):
+    async def run_forever(self) -> None:
+        ...
+
+
+async def run_all_forever(runnables: Iterable[LifetimeRunnable]) -> None:
+    async def run_with_error_handling(runnable: LifetimeRunnable) -> None:
+        try:
+            await runnable.run_forever()
+        except Exception as e:
+            print("Task exception: {}".format(e))
+            print(traceback.format_exc())
+
+    await asyncio.wait(
+        [
+            asyncio.create_task(run_with_error_handling(runnable))
+            for runnable in runnables
+        ]
+    )
