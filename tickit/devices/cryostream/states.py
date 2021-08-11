decbcf230f6f1c5236d9a55b1ee6894cb359112d
Martin Gomez, Paula (DLSLtd,RAL,LSCI) <paula.martin-gomez@diamond.ac.uk>
2021-08-11 15:56:55 +0000

    Cryostream initial implementation

---
+++ tickit/devices/cryostream/states.py
@@ -0,0 +1,92 @@
+from enum import IntEnum
+
+
+class RunModes(IntEnum):
+    STARTUP = 0
+    STARTUPFAIL = 1
+    STARTUPOK = 2
+    RUN = 3
+    SETUP = 4
+    SHUTDOWNOK = 5
+    SHUTDOWNFAIL = 6
+
+
+class PhaseIds(IntEnum):
+    RAMP = 0
+    COOL = 1
+    PLAT = 2
+    HOLD = 3
+    END = 4
+    PURGE = 5
+
+
+class AlarmCodes(IntEnum):
+    """
+    Each alarm has an alarm level, which is not included in the status packet but
+    may be read from the table below
+    """
+
+    NO_ERRORS = 0
+    STOP_PRESSED = 1
+    STOP_COMMAND = 2
+    END_COMPLETE = 3
+    PURGE_COMPLETE = 4
+    TEMP_WARNING = 5
+    PRESSURE_WARNING = 6
+    CHECK_VACUUM = 7
+    SELF_CHECK_FAIL = 8
+    FLOW_RATE = 9
+    TEMP_CONTROL_ERROR = 10
+    GAS_TYPE_ERROR = 11
+    TEMP_READING_ERROR = 12
+    SUCT_TEMP_ERROR = 13
+    SENSOR_FAIL = 14
+    BROWNOUT = 15
+    SINK_OVERHEAT = 16
+    PSU_OVERHEAT = 17
+    POWER_LOSS = 18
+    COLHEAD_TOO_COLD = 19
+    COLHEAD_TIMEOUT = 20
+    CRYODRIVE_NOT_FOUND = 21
+    CRYODRIVE_ERROR = 22
+    NO_NITROGEN = 23
+    NO_HELIUM = 24
+    VAC_GAUGE_FAIL = 25
+    VAC_READING_ERROR = 26
+    RS232_ERROR = 27
+    COLDHEAD_TEMP_WARNING = 28
+    COLDHEAD_TEMP_ERROR = 29
+    DO_NOT_OPEN_CRYOSTAT_L2 = 30
+    DO_NOT_OPEN_CRYOSTAT_L3 = 31
+    UNPLUG_XTAL_SENSOR = 32
+    CRYOSTAT_OPEN = 33
+    CRYOSTAT_OPEN_TIMEOUT = 34
+    HIGH_TEMP_WARNING = 35
+    HIGH_TEMP_ERROR = 36
+    CRYODRIVE_T_SENSOR_FAULT = 37
+    CRYODRIVE_P_SENSOR_FAULT = 38
+    CRYODRIVE_LOW_T_TRIP = 39
+    CRYODRIVE_HIGH_T_TRIP = 40
+    CRYODRIVE_LOW_P_TRIP = 41
+    CRYODRIVE_HIGH_T_WARNING = 42
+    CRYODRIVE_LOW_P_WARNING = 43
+    CONNECT_GAS_SUPPLY = 44
+    AUTOFILL_FAULT = 45
+    AUTOFILL_ABOUT_TO_FILL = 46
+    AUTOFILL_FILLING = 47
+    COLLAR_TEMP_ERROR = 48
+    COLDHEAD_ERROR = 49
+    TURBO_FLOW = 50
+    HE_SELECTED = 51
+    CRYODRIVE_NOT_READY = 52
+    REGEN_REQUIRED = 53
+    REGEN_COMPLETE = 54
+    CONNECT_VACUUM = 55
+    DISCONNECT_VACUUM = 56
+
+
+class HardwareType(IntEnum):
+    PLUS_SYSTEM = 1  # Max temp 500 K (Cryostream, Cobra and Smartstream)
+    HAS_CRYOSHUTTER = 2  # 700 series Cryostream and Cobra
+    SERIES_800 = 3
+    HAS_AUTOFILL = 4  # 800 series Cryostream from firmware version 150
---
+++ tickit/devices/cryostream/status.py
@@ -0,0 +1,134 @@
+import struct
+from dataclasses import dataclass
+
+
+@dataclass
+class Status:
+    length: int
+    type_status: int
+    gas_set_point: int
+    gas_temp: int
+    gas_error: int
+    run_mode: int
+    phase_id: int
+    ramp_rate: int
+    target_temp: int
+    evap_temp: int
+    suct_temp: int
+    remaining: int
+    gas_flow: int
+    gas_heat: int
+    evap_heat: int
+    suct_heat: int
+    line_pressure: int
+    alarm_code: int
+    run_time: int
+    controller_number: int
+    software_version: int
+    evap_adjust: int
+    status_bytes_string: str = ">BBHHhBBHHHHHBBBBBBHHBB"
+
+    def pack(self) -> bytes:
+        status_bytes = struct.pack(
+            self.status_bytes_string,
+            self.length,
+            self.type_status,
+            self.gas_set_point,
+            self.gas_temp,
+            self.gas_error,
+            self.run_mode,
+            self.phase_id,
+            self.ramp_rate,
+            self.target_temp,
+            self.evap_temp,
+            self.suct_temp,
+            self.remaining,
+            self.gas_flow,
+            self.gas_heat,
+            self.evap_heat,
+            self.suct_heat,
+            self.line_pressure,
+            self.alarm_code,
+            self.run_time,
+            self.controller_number,
+            self.software_version,
+            self.evap_adjust,
+        )
+
+        return status_bytes
+
+
+@dataclass
+class ExtendedStatus:
+    """
+    Chars have a size of 1 byte, shorts have a size of 2 bytes
+    All temperatures are in centi-Kelvin 80K is reported as 8000
+    """
+
+    length: int
+    type_status: int
+    gas_set_point: int
+    gas_temp: int
+    gas_error: int
+    run_mode: int
+    phase_id: int
+    ramp_rate: int
+    target_temp: int
+    evap_temp: int
+    suct_temp: int
+    remaining: int
+    gas_flow: int
+    gas_heat: int
+    evap_heat: int
+    suct_heat: int
+    line_pressure: int
+    alarm_code: int
+    run_time: int
+    controller_number: int
+    software_version: int
+    evap_adjust: int
+    turbo_mode: int
+    hardware_type: int
+    shutter_state: int  # 800 series do not support CryoShutter
+    shutter_time: int  # 800 series do not support CryoShutter
+    avg_gas_heat: int
+    avg_suct_heat: int
+    time_to_fill: int
+    total_hours: int
+    extended_packet_string: str = ">BBHHhBBHHHHHBBBBBBHHBBBBBBBBHH"
+
+    def pack(self) -> bytes:
+        extended_status_bytes = struct.pack(
+            self.extended_packet_string,
+            self.length,
+            self.type_status,
+            self.gas_set_point,
+            self.gas_temp,
+            self.gas_error,
+            self.run_mode,
+            self.phase_id,
+            self.ramp_rate,
+            self.target_temp,
+            self.evap_temp,
+            self.suct_temp,
+            self.remaining,
+            self.gas_flow,
+            self.gas_heat,
+            self.evap_heat,
+            self.suct_heat,
+            self.line_pressure,
+            self.alarm_code,
+            self.run_time,
+            self.controller_number,
+            self.software_version,
+            self.evap_adjust,
+            self.turbo_mode,
+            self.hardware_type,
+            self.shutter_state,
+            self.shutter_time,
+            self.avg_gas_heat,
+            self.avg_suct_heat,
+            self.time_to_fill,
+            self.total_hours,
+        )
+        return extended_status_bytes
