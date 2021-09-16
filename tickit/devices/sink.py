305f8270d648115172c4810c942e372bd5051a7f
O'Donnell, Garry (DLSLtd,RAL,LSCI) <garry.o'donnell@diamond.ac.uk>
2021-09-16 10:13:56 +0100

    Fix sink docstrings

--- tickit/devices/sink.py
+++ tickit/devices/sink.py
@@ -9,7 +9,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 class Sink(ConfigurableDevice):
-    """A simple device which can take any input and produces no output"""
+    """A simple device which can take any input and produces no output."""
 
     #: A typed mapping containing the 'input' input value
     Inputs: TypedDict = TypedDict("Inputs", {"input": Any})
@@ -17,20 +17,20 @@ class Sink(ConfigurableDevice):
     Outputs: TypedDict = TypedDict("Outputs", {})
 
     def __init__(self) -> None:
-        """A constructor of the sink, with no arguments"""
+        """A constructor of the sink, with no arguments."""
         pass
 
     def update(self, time: SimTime, inputs: Inputs) -> DeviceUpdate[Outputs]:
-        """The update method which prints the inputs to console and produces no outputs
+        """The update method which logs the inputs at debug level and produces no outputs.
 
         Args:
-            time (SimTime): The current simulation time (in nanoseconds)
-            inputs (State): A mapping of inputs to the device and their values
+            time (SimTime): The current simulation time (in nanoseconds).
+            inputs (State): A mapping of inputs to the device and their values.
 
         Returns:
             DeviceUpdate[Outputs]:
                 The produced update event which never contains any changes, and never
-                requests a callback
+                requests a callback.
         """
         LOGGER.debug("Sunk {}".format({k: v for k, v in inputs.items()}))
         return DeviceUpdate(Sink.Outputs(), None)
