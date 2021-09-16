fc2db9ad1ab4892978461f4c5835edc1794343d0
O'Donnell, Garry (DLSLtd,RAL,LSCI) <garry.o'donnell@diamond.ac.uk>
2021-09-16 10:11:58 +0100

    Fix source docstrings

--- tickit/devices/source.py
+++ tickit/devices/source.py
@@ -9,7 +9,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 class Source(ConfigurableDevice):
-    """A simple device which produces a pre-configured value"""
+    """A simple device which produces a pre-configured value."""
 
     #: An empty typed mapping of device inputs
     Inputs: TypedDict = TypedDict("Inputs", {})
@@ -17,25 +17,24 @@ class Source(ConfigurableDevice):
     Outputs: TypedDict = TypedDict("Outputs", {"value": Any})
 
     def __init__(self, value: Any) -> None:
-        """A constructor of the source, which takes the pre-configured value
+        """A constructor of the source, which takes the pre-configured output value.
 
         Args:
-            value (Any): A pre-configured output value
+            value (Any): A pre-configured output value.
         """
         self.value = value
 
     def update(self, time: SimTime, inputs: Inputs) -> DeviceUpdate[Outputs]:
-        """The update method which produces the pre-configured value
+        """The update method which produces the pre-configured output value.
 
         Args:
-            time (SimTime): The current simulation time (in nanoseconds)
-            inputs (State): A mapping of inputs to the device and their values
+            time (SimTime): The current simulation time (in nanoseconds).
+            inputs (State): A mapping of inputs to the device and their values.
 
         Returns:
             DeviceUpdate[Outputs]:
                 The produced update event which contains the pre-configured value, and
-                never requests a callback
+                never requests a callback.
         """
-
         LOGGER.debug("Sourced {}".format(self.value))
         return DeviceUpdate(Source.Outputs(value=self.value), None)
