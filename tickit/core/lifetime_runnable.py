743e600e151698d55cd3bbc7ccb2f35f836b7b34
O'Donnell, Garry (DLSLtd,RAL,LSCI) <garry.o'donnell@diamond.ac.uk>
2021-09-16 13:10:47 +0100

    Fix lifetime_runnable docstrings

--- tickit/core/lifetime_runnable.py
+++ tickit/core/lifetime_runnable.py
@@ -7,23 +7,24 @@ from tickit.utils.compat.typing_compat import Protocol, runtime_checkable
 
 @runtime_checkable
 class LifetimeRunnable(Protocol):
-    """An interface for types which implement an awaitable run_forever method"""
+    """An interface for types which implement an awaitable run_forever method."""
 
     async def run_forever(self) -> None:
+        """An asynchronous method which may run indefinitely."""
         pass
 
 
 def run_all(runnables: Iterable[LifetimeRunnable]) -> List[asyncio.Task]:
-    """Asynchronously runs the run_forever method of each lifetime runnable
+    """Asynchronously runs the run_forever method of each lifetime runnable.
 
     Creates and runs an asyncio task for the run_forever method of each lifetime
     runnable. Calls to the run_forever method are wrapped with an error handler.
 
     Args:
-        runnables: An iterable of objects which implement run_forever
+        runnables: An iterable of objects which implement run_forever.
 
     Returns:
-        List[asyncio.Task]: A list of asyncio tasks for the runnables
+        List[asyncio.Task]: A list of asyncio tasks for the runnables.
     """
 
     async def run_with_error_handling(runnable: LifetimeRunnable) -> None:
@@ -41,13 +42,13 @@ def run_all(runnables: Iterable[LifetimeRunnable]) -> List[asyncio.Task]:
 
 
 async def run_all_forever(runnables: Iterable[LifetimeRunnable]) -> None:
-    """Asynchronously runs the run_forever method of each lifetime runnable
+    """Asynchronously runs the run_forever method of each lifetime runnable.
 
     Creates and runs an asyncio task for the run_forever method of each lifetime
     runnable. Calls to the run_forever method are wrapped with an error handler.
     This function blocks until all run_forever methods have completed.
 
     Args:
-        runnables: An iterable of objects which implement run_forever
+        runnables: An iterable of objects which implement run_forever.
     """
     await asyncio.wait(run_all(runnables))
