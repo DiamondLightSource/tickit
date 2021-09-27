3b3af4207e111bf4d6782001e4dd5bcee39eb1ae
AngusCollett <57042141+AngusCollett@users.noreply.github.com>
2021-09-27 16:14:40 +0100

    Revert commenting-out
    It was not supposed to be committed.

--- docs/conf.py
+++ docs/conf.py
@@ -117,7 +117,7 @@ rst_epilog = """
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = "sphinx_rtd_theme_github_versions"
+html_theme = "sphinx_rtd_theme_github_versions"
 
 # Options for the sphinx rtd theme, use DLS blue
 html_theme_options = dict(style_nav_header_background="rgb(7, 43, 93)")
