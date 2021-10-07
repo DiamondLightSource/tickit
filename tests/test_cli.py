a9dfb914a00a1ba275028ed369a4a1b2cf3e77b2
garryod <garry.euan.odonnell@ntlworld.com>
2021-10-07 13:33:46 +0100

    Fix CLI tests by mocking tickit.cli.run_all_forever

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,20 @@
 from pathlib import Path
 
 import mock
+import pytest
 from click.testing import CliRunner, Result
+from mock.mock import AsyncMock
 
-from tickit.cli import main
+
+@pytest.fixture
+def mock_run_all_forever():
+    with mock.patch("tickit.cli.run_all_forever") as mock_run_all_forever:
+        yield mock_run_all_forever
 
 
 def test_cli_set_loggging_level():
+    from tickit.cli import main
+
     runner: CliRunner = CliRunner()
     with mock.patch("logging.basicConfig") as mock_basicConfig:
         result: Result = runner.invoke(main, args=["--log-level", "INFO"])
@@ -14,7 +22,9 @@ def test_cli_set_loggging_level():
         mock_basicConfig.assert_called_with(level="INFO")
 
 
-def test_component_command():
+def test_component_command(mock_run_all_forever: AsyncMock):
+    from tickit.cli import main
+
     runner: CliRunner = CliRunner()
 
     test_config_path: str = (
@@ -23,16 +33,17 @@ def test_component_command():
         .__str__()
     )
 
-    with mock.patch("asyncio.run") as run_patch:
-        result: Result = runner.invoke(
-            main, args=["component", "rand_tramp", test_config_path]
-        )
-        assert result.exit_code == 0
+    result: Result = runner.invoke(
+        main, args=["component", "rand_tramp", test_config_path]
+    )
+    assert result.exit_code == 0
 
-    run_patch.assert_called()
+    mock_run_all_forever.assert_called_once()
 
 
-def test_scheduler():
+def test_scheduler(mock_run_all_forever: AsyncMock):
+    from tickit.cli import main
+
     runner: CliRunner = CliRunner()
 
     test_config_path: str = (
@@ -41,14 +52,15 @@ def test_scheduler():
         .__str__()
     )
 
-    with mock.patch("asyncio.run") as run_patch:
-        result: Result = runner.invoke(main, args=["scheduler", test_config_path])
-        assert result.exit_code == 0
+    result: Result = runner.invoke(main, args=["scheduler", test_config_path])
+    assert result.exit_code == 0
 
-    run_patch.assert_called()
+    mock_run_all_forever.assert_awaited_once()
 
 
-def test_all():
+def test_all(mock_run_all_forever: AsyncMock):
+    from tickit.cli import main
+
     runner: CliRunner = CliRunner()
 
     test_config_path: str = (
@@ -57,8 +69,7 @@ def test_all():
         .__str__()
     )
 
-    with mock.patch("asyncio.run") as run_patch:
-        result: Result = runner.invoke(main, args=["all", test_config_path])
-        assert result.exit_code == 0
+    result: Result = runner.invoke(main, args=["all", test_config_path])
+    assert result.exit_code == 0
 
-    run_patch.assert_called()
+    mock_run_all_forever.assert_awaited_once()
