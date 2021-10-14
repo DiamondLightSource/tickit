c3148282c919399340efa08983175c0ab2454d8e
Angus Collett <angus.collett@diamond.ac.uk>
2021-10-14 08:31:26 +0100

    Add tests for TcpServer

---
+++ tests/adapters/servers/test_tcp.py
@@ -0,0 +1,89 @@
+from asyncio import StreamReader, StreamWriter
+from typing import AsyncIterable, Callable
+
+import pytest
+from mock import ANY, MagicMock, Mock, patch
+from mock.mock import AsyncMock, create_autospec
+
+from tickit.adapters.servers.tcp import TcpServer
+
+
+@pytest.fixture
+def tcp_server() -> TcpServer:
+    return TcpServer()
+
+
+@pytest.fixture
+def on_connect() -> Callable[[], AsyncIterable[bytes]]:
+    async def on_connect():
+        yield b"hello"
+
+    return on_connect
+
+
+@pytest.fixture
+def handler() -> Callable[[bytes], AsyncIterable[bytes]]:
+    async def _handler(message: bytes) -> Callable[[], AsyncIterable[bytes]]:
+        async def async_iterable() -> AsyncIterable[bytes]:
+            yield b"hello"
+            yield b""
+            yield b"blue"
+            yield b"world"
+            yield b""
+
+        return async_iterable
+
+    return _handler  # type: ignore
+
+
+@pytest.fixture
+def mock_start_server():
+    with patch("asyncio.start_server") as mock:
+        yield mock
+
+
+@pytest.mark.asyncio
+async def test_TcpServer_run_forever_method(
+    tcp_server: TcpServer,
+    on_connect: Callable[[], AsyncIterable[bytes]],
+    handler: Callable[[bytes], AsyncIterable[bytes]],
+    mock_start_server: Mock,
+):
+
+    mock_start_server.return_value = AsyncMock()
+
+    await tcp_server.run_forever(on_connect, handler)
+
+    mock_start_server.assert_awaited_once_with(ANY, tcp_server.host, tcp_server.port)
+    mock_start_server.return_value.serve_forever.assert_awaited_once()
+
+
+@pytest.fixture
+def mock_stream_reader() -> Mock:
+    mock: MagicMock = create_autospec(StreamReader, instance=True)
+    mock.read = AsyncMock(side_effect=(b"hello", b"world", b""))
+    return mock
+
+
+@pytest.fixture
+def mock_stream_writer() -> Mock:
+    mock: MagicMock = create_autospec(StreamWriter, instance=True)
+    mock.is_closing = Mock(side_effect=[False, True, True])
+    mock.write = Mock()
+    mock.drain = AsyncMock()
+    return mock
+
+
+@pytest.mark.asyncio
+async def test_TcpServer_generate_handle_function_method(
+    tcp_server: TcpServer,
+    on_connect: Callable[[], AsyncIterable[bytes]],
+    handler: Callable[[bytes], AsyncIterable[bytes]],
+    mock_stream_reader: Mock,
+    mock_stream_writer: Mock,
+):
+    handle_function = tcp_server.generate_handle_function(on_connect, handler)
+
+    assert handle_function is not None
+
+    await handle_function(mock_stream_reader, mock_stream_writer)
