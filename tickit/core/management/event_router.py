3df8e6917d2312d7b0bff73441c74ff7dc45986b
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2021-10-19 16:27:38 +0100

    Fix event_router indentation

--- tickit/core/management/event_router.py
+++ tickit/core/management/event_router.py
@@ -119,7 +119,7 @@ class InverseWiring(Default_InverseWiring_Struct):
         Returns:
             InverseWiring: A mapping of component input ports to component output ports.
         """
-       return cls({config.name: config.inputs for config in configs})
+        return cls({config.name: config.inputs for config in configs})
 
 
 class EventRouter:
