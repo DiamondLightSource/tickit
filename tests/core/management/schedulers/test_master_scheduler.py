2d8ec4b89a0feff9a7d0acaca9e94d40483cdce2
vcv88373 <angus.collett@diamond.ac.uk>
2021-10-20 09:01:36 +0100

    Add working tests for multiple methods of MasterScheduler

--- tests/core/management/schedulers/test_master_scheduler.py
+++ tests/core/management/schedulers/test_master_scheduler.py
@@ -1,4 +1,6 @@
-from typing import Iterable
+import logging
+from time import time
+from typing import Awaitable, Iterable, Set
 
 import pytest
 from mock import AsyncMock, Mock, create_autospec, patch
@@ -7,6 +9,7 @@ from tickit.core.management.event_router import Wiring
 from tickit.core.management.schedulers.master import MasterScheduler
 from tickit.core.management.ticker import Ticker
 from tickit.core.state_interfaces.state_interface import StateConsumer, StateProducer
+from tickit.core.typedefs import ComponentID, SimTime
 
 
 @pytest.fixture
@@ -27,7 +30,8 @@ def mock_state_producer() -> Mock:
 @pytest.fixture
 def patch_ticker() -> Iterable[Mock]:
     with patch("tickit.core.management.schedulers.base.Ticker", autospec=True) as mock:
-        mock.return_value = AsyncMock(spec_set=Ticker)
+        mock.return_value = AsyncMock(spec=Ticker)
+        mock.return_value.time = SimTime(41)
         yield mock
 
 
@@ -50,16 +54,102 @@ def test_master_scheduler_constructor_and_setup_methods(
 
 
 @pytest.fixture
-def patch_asyncio() -> Iterable[Mock]:
+def patch_asyncio_wait() -> Iterable[Mock]:
+    """A patch over the method asyncio.wait.
+
+    This fixture patches over asyncio.wait to prevent the test from waiting
+    indefinitely.The method is replaced with a new callable that simulates the
+    outcome of the asyncio.wait method in order to cover most of the tested function.
+    """
+
+    class wait:
+        def __init__(self):
+            self.call_count = 0
+            self.threshold = 3
+
+        async def __call__(self, awaitables: Set[Awaitable], return_when):
+            """Simulates the call to asyncio.wait(return_when=FIRST_COMPLETED).
+
+            The call to wait is a (deliberate) race condition. To simulate this and
+            test all parts of the code, this method switches it's response depending
+            on how many times it's been called.
+            """
+            for aw in awaitables:
+                await aw
+
+            if self.call_count < self.threshold:
+                self.call_count += 1
+                return [list(awaitables)[0]], None
+            else:
+                self.call_count += 1
+                return [list(awaitables)[1]], None
+
     with patch(
-        "tickit.core.management.schedulers.master.asyncio", autospec=True
+        "tickit.core.management.schedulers.master.asyncio.wait",
+        new_callable=wait,
+    ) as mock:
+        yield mock
+
+
+@pytest.fixture
+def patch_asyncio_Event() -> Iterable[Mock]:
+    """A patch over the asyncio.Event class. Prevents warnings from being raised."""
+    with patch(
+        "tickit.core.management.schedulers.master.asyncio.Event", autospec=True
+    ) as mock:
+        yield mock
+
+
+@pytest.fixture
+def patch_asyncio_sleep() -> Iterable[Mock]:
+    with patch(
+        "tickit.core.management.schedulers.master.asyncio.sleep", autospec=True
     ) as mock:
         yield mock
 
 
 @pytest.mark.asyncio
 async def test_master_scheduler_run_tick_methods(
-    master_scheduler: MasterScheduler, patch_asyncio
+    master_scheduler: MasterScheduler,
+    patch_asyncio_wait,
+    patch_asyncio_Event,
+    patch_asyncio_sleep,
 ):
+    master_scheduler.add_wakeup(ComponentID("foo"), SimTime(42))
+    master_scheduler.add_wakeup(ComponentID("bar"), SimTime(52))
+    master_scheduler.add_wakeup(ComponentID("baz"), SimTime(62))
+    master_scheduler.add_wakeup(ComponentID("boo"), SimTime(72))
     await master_scheduler._run_initial_tick()
     await master_scheduler._run_tick()
+    assert master_scheduler.wakeups == {"bar": 52, "baz": 62, "boo": 72}
+    await master_scheduler._run_tick()
+    assert master_scheduler.wakeups == {"baz": 62, "boo": 72}
+    await master_scheduler._run_tick()
+    assert master_scheduler.wakeups == {"boo": 72}
+    await master_scheduler._run_tick()
+    assert master_scheduler.wakeups == {"boo": 72}
+
+
+@pytest.fixture
+def patch_time_ns() -> Iterable[Mock]:
+    class frozen_clock:
+        def __init__(self):
+            self.time = 1000
+
+        def __call__(self):
+            return self.time
+
+    with patch(
+        "tickit.core.management.schedulers.master.time_ns", new_callable=frozen_clock
+    ) as mock:
+        yield mock
+
+
+@pytest.mark.asyncio
+async def test_master_scheduler_schedule_interrupt_method(
+    master_scheduler: MasterScheduler, patch_time_ns
+):
+    await master_scheduler._run_initial_tick()
+    assert master_scheduler.wakeups == {}
+    await master_scheduler.schedule_interrupt(ComponentID("foo"))
+    assert master_scheduler.wakeups == {"foo": master_scheduler.ticker.time}
