55ee50e5708248fbf840f309ee8496ec9c626028
AngusCollett <AngusCollett@users.noreply.github.com>
2021-11-02 14:30:08 +0000

    deploy: 503c3386641bded5554466a9c4c17a21b661c22c

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.pre2+5.g02e9a34',
+    VERSION: '0.1.1.pre2+6.g503c338',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
