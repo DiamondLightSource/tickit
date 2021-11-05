566fad2dfbbd6902e8e38d6d25a22f0eb15c0c6c
Angus Collett <angus.collett@diamond.ac.uk>
2021-11-05 09:48:15 +0000

    Refactor tests to use fixture and conform to style

--- tests/utils/test_byte_format.py
+++ tests/utils/test_byte_format.py
@@ -4,22 +4,19 @@ from tickit.utils.byte_format import ByteFormat
 
 
 @pytest.fixture
-def byteformat() -> ByteFormat:
+def byte_format() -> ByteFormat:
     return ByteFormat(b"%b/r/n")
 
 
-def test_byte_format_constructor():
-    ByteFormat(b"%b/r/n")
+def test_byte_format_constructor(byte_format: ByteFormat):
+    pass
 
 
-def test_byteformat_serializer():
-    byteformat = ByteFormat(b"%b/r/n")
-    serialized = byteformat.serialize()
-
+def test_byteformat_serializer(byte_format: ByteFormat):
+    serialized = byte_format.serialize()
     assert "%b/r/n" == serialized
 
 
-def test_byteformat_deserializer(byteformat):
-    deserialized = byteformat.deserialize("%b/r/n")
-
+def test_byteformat_deserializer(byte_format: ByteFormat):
+    deserialized = byte_format.deserialize("%b/r/n")
     assert ByteFormat(b"%b/r/n") == deserialized
