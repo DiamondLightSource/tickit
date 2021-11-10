30fdba25744707d90bdf941960b39e867cb734ac
AngusCollett <AngusCollett@users.noreply.github.com>
2021-11-10 09:26:38 +0000

    deploy: 86fd829c0614f6efa9c3f7958b922ad7a12e5f50

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.pre2+8.g09afa3e',
+    VERSION: '0.1.1.pre2+9.g86fd829',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
