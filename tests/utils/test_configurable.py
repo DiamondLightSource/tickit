43eecd4e8dc66fbdaf44a2750f0bafca2ca5aec4
Ollie Copping <oliver.copping@diamond.ac.uk>
2021-11-16 10:25:44 +0000

    Fixed two failing tests

--- tests/utils/test_configurable.py
+++ tests/utils/test_configurable.py
@@ -31,7 +31,7 @@ def test_tagged_union_deserializes():
 
 def test_deserialization_schema():
     assert deserialization_schema(MyBase) == {
-        "$schema": "http://json-schema.org/draft/2019-09/schema#",
+        "$schema": "http://json-schema.org/draft/2020-12/schema#",
         "additionalProperties": False,
         "maxProperties": 1,
         "minProperties": 1,
