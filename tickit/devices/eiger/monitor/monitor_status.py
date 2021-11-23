f2e3a6318da202cdf4c851f61e72c276e34f265b
Ollie Copping <oliver.copping@diamond.ac.uk>
2021-11-23 10:57:49 +0000

    Removed __setitem__ functions from sub device status objects

--- tickit/devices/eiger/monitor/monitor_status.py
+++ tickit/devices/eiger/monitor/monitor_status.py
@@ -23,6 +23,3 @@ class MonitorStatus:
                 "metadata": field_.metadata,
             }
         return f[key]
-
-    def __setitem__(self, key: str, value: Any) -> None:  # noqa: D105
-        self.__dict__[key] = value
--- tickit/devices/eiger/stream/stream_status.py
+++ tickit/devices/eiger/stream/stream_status.py
@@ -25,6 +25,3 @@ class StreamStatus:
                 "metadata": field_.metadata,
             }
         return f[key]
-
-    def __setitem__(self, key: str, value: Any) -> None:  # noqa: D105
-        self.__dict__[key] = value
