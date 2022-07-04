515814e8d1f46c23f70190bd9bf1cabbf4677858
Callum Forrester <callum.forrester@diamond.ac.uk>
2022-07-04 14:57:47 +0100

    WIP on IOC singleton for EpicsAdapter

--- tickit/adapters/epicsadapter.py
+++ tickit/adapters/epicsadapter.py
@@ -1,15 +1,20 @@
 import asyncio
+import itertools
+import logging
 import os
 import re
+import threading
 from abc import abstractmethod
 from dataclasses import dataclass
 from tempfile import NamedTemporaryFile
-from typing import Any, Callable, Dict
+from typing import Any, Callable, Dict, List
 
 from softioc import asyncio_dispatcher, builder, softioc
 
 from tickit.core.adapter import Adapter, RaiseInterrupt
 from tickit.core.device import Device
+
+LOGGER = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
@@ -28,6 +33,47 @@ class OutputRecord:
     name: str
 
 
+class IocSingleton:
+    _ioc_name: str
+    _reg: List[int]
+    _id_counter: itertools.count
+
+    def __init__(self, ioc_name: str) -> None:
+        self._ioc_name = ioc_name
+        self._reg = []
+        self._id_counter = itertools.count()
+
+    def register_adapter(self) -> int:
+        adapter_id = next(self._id_counter)
+        LOGGER.info(f"New IOC adapter registering with ID: {adapter_id}")
+        self._reg.append(adapter_id)
+        return adapter_id
+
+    def notify_adapter_ready(self, adapter_id: int) -> None:
+        self._reg.remove(adapter_id)
+        LOGGER.info(f"IOC adapter #{adapter_id} reports ready")
+        if not self._reg:
+            LOGGER.info("All registered adapters are ready, starting IOC")
+            self.build_ioc()
+
+    def build_ioc(self) -> None:
+        """Builds an EPICS python soft IOC for the adapter."""
+        LOGGER.info("Initializing database")
+        builder.SetDeviceName(self._ioc_name)
+        softioc.devIocStats(self._ioc_name)
+        builder.LoadDatabase()
+
+        LOGGER.info("Starting IOC")
+        event_loop = asyncio.get_event_loop()
+        dispatcher = asyncio_dispatcher.AsyncioDispatcher(event_loop)
+        softioc.iocInit(dispatcher)
+        softioc.dbl()
+        LOGGER.info("IOC started")
+
+
+_IOC_SINGLETON = IocSingleton("TICKIT_IOC")
+
+
 class EpicsAdapter(Adapter):
     """An adapter implementation which acts as an EPICS IOC."""
 
@@ -41,6 +87,7 @@ class EpicsAdapter(Adapter):
         self.db_file = db_file
         self.ioc_name = ioc_name
         self.interrupt_records: Dict[InputRecord, Callable[[], Any]] = {}
+        self.ioc_num = _IOC_SINGLETON.register_adapter()
 
     def link_input_on_interrupt(
         self, record: InputRecord, getter: Callable[[], Any]
@@ -76,23 +123,11 @@ class EpicsAdapter(Adapter):
         softioc.dbLoadDatabase(out.name, substitutions=f"device={self.ioc_name}")
         os.unlink(out.name)
 
-    def build_ioc(self) -> None:
-        """Builds an EPICS python soft IOC for the adapter."""
-        builder.SetDeviceName(self.ioc_name)
-
-        self.load_records_without_DTYP_fields()
-        self.on_db_load()
-
-        softioc.devIocStats(self.ioc_name)
-
-        builder.LoadDatabase()
-        event_loop = asyncio.get_event_loop()
-        dispatcher = asyncio_dispatcher.AsyncioDispatcher(event_loop)
-        softioc.iocInit(dispatcher)
-
     async def run_forever(
         self, device: Device, raise_interrupt: RaiseInterrupt
     ) -> None:
         """Runs the server continously."""
         await super().run_forever(device, raise_interrupt)
-        self.build_ioc()
+        self.load_records_without_DTYP_fields()
+        self.on_db_load()
+        _IOC_SINGLETON.notify_adapter_ready(self.ioc_num)
