9c1ea3b60c28ec363368ab987df78d83d6eb600a
Matthew Pritchard <matthew.pritchard@diamond.ac.uk>
2022-08-08 13:36:26 +0100

    #78: Fix formatting of BeheadingInterpreter

--- tickit/adapters/interpreters/command/command_interpreter.py
+++ tickit/adapters/interpreters/command/command_interpreter.py
@@ -110,15 +110,16 @@ class BeheadingInterpreter(Interpreter[AnyStr]):
     """
 
     def __init__(self, interpreter: Interpreter[AnyStr], header_size: int) -> None:
-        """
+        """A decorator for an interpreter which strips a header from a message.
+
         Args:
             interpreter (Interpreter): The interpreter the message is passed on to
                 after the header is stripped.
             header_size (int): The number of characters in the header.
         """
         super().__init__()
-        self.interpreter = interpreter
-        self.header_size = header_size
+        self.interpreter: Interpreter[AnyStr] = interpreter
+        self.header_size: int = header_size
 
     async def handle(
         self, adapter: Adapter, message: AnyStr
@@ -134,5 +135,5 @@ class BeheadingInterpreter(Interpreter[AnyStr]):
                 A tuple of the asynchronous iterable of reply messages and a flag
                 indicating whether an interrupt should be raised by the adapter.
         """
-        message = message[self.header_size:]
+        message = message[self.header_size :]
         return await self.interpreter.handle(adapter, message)
