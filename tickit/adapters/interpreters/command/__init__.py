da5451bbb431281cbe395900787f5cda502db84e
Matthew Pritchard <matthew.pritchard@diamond.ac.uk>
2022-08-09 10:56:37 +0100

    #78: Remove BeheadingInterpreter from command __init__.py

--- tickit/adapters/interpreters/command/__init__.py
+++ tickit/adapters/interpreters/command/__init__.py
@@ -1,7 +1,4 @@
-from tickit.adapters.interpreters.command.command_interpreter import (
-    BeheadingInterpreter,
-    CommandInterpreter,
-)
+from tickit.adapters.interpreters.command.command_interpreter import CommandInterpreter
 from tickit.adapters.interpreters.command.regex_command import RegexCommand
 
-__all__ = ["CommandInterpreter", "RegexCommand", "BeheadingInterpreter"]
+__all__ = ["CommandInterpreter", "RegexCommand"]
