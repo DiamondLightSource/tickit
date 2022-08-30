344daddfb8ed2bff7b8d007134183cd72b4efa0f
Matthew Pritchard <matthew.pritchard@diamond.ac.uk>
2022-08-30 15:21:59 +0100

    #88: Add JoiningInterpreter

---
+++ tests/adapters/interpreters/wrappers/test_joining_interpreter.py
@@ -0,0 +1,17 @@
+import pytest
+from mock import AsyncMock
+
+from tickit.adapters.interpreters.wrappers import JoiningInterpreter
+
+
+@pytest.mark.asyncio
+async def test_joining_interpreterconcatenates_as_expected():
+    async def multi_resp(msgs):
+        for msg in msgs:
+            yield msg
+
+    mock_interpreter = AsyncMock()
+    mock_interpreter.handle.return_value = multi_resp(["one", "two", "three"]), True
+    joining_interpreter = JoiningInterpreter(mock_interpreter, response_delimiter="-")
+    result, _ = await joining_interpreter.handle(AsyncMock(), "test")
+    assert [msg async for msg in result] == ["one-two-three"]
--- tickit/adapters/interpreters/wrappers/__init__.py
+++ tickit/adapters/interpreters/wrappers/__init__.py
@@ -1,5 +1,6 @@
 from tickit.adapters.interpreters.wrappers.beheading_interpreter import (
     BeheadingInterpreter,
 )
+from tickit.adapters.interpreters.wrappers.joining_interpreter import JoiningInterpreter
 
-__all__ = ["BeheadingInterpreter"]
+__all__ = ["BeheadingInterpreter", "JoiningInterpreter"]
