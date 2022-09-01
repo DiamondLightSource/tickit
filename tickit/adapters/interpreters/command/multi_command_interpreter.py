490a25ab6d9f3606b2e2d0e88b4a78ac0c75292f
Matthew Pritchard <matthew.pritchard@diamond.ac.uk>
2022-09-01 10:17:30 +0100

    #77: Refactor to remove some typing issues

--- tickit/adapters/interpreters/command/multi_command_interpreter.py
+++ tickit/adapters/interpreters/command/multi_command_interpreter.py
@@ -1,12 +1,20 @@
-import re
 from inspect import getmembers
-from typing import AnyStr, AsyncIterable, Callable, Dict, List, Tuple, get_type_hints
+from typing import (
+    AnyStr,
+    AsyncIterable,
+    Callable,
+    List,
+    Optional,
+    Tuple,
+    cast,
+    get_type_hints,
+)
 
-from tickit.adapters.interpreters.utils import wrap_as_async_iterable
+from tickit.adapters.interpreters.command.command_interpreter import Command
 from tickit.core.adapter import Adapter, Interpreter
 
 
-class MultiCommandInterpreter(Interpreter[AnyStr]):
+class MultiCommandInterpreter(Interpreter[AnyStr, AnyStr]):
     """An interpreter which routes to commands registered to adapter methods.
 
     An interpreter which attempts to parse commands from within a message. Matching
@@ -26,31 +34,6 @@ class MultiCommandInterpreter(Interpreter[AnyStr]):
         super().__init__()
         self.ignore_whitespace = ignore_whitespace
 
-    @staticmethod
-    def _get_longest_regex_match(regexes: List[str], message: str):
-        longest_match = None
-        for regex in regexes:
-            match = re.match(regex, message)
-            if match is not None:
-                if longest_match is None:
-                    longest_match = match
-                    continue
-                elif match.end() > longest_match.end():
-                    longest_match = match
-        return longest_match
-
-    @staticmethod
-    def _get_args_from_command_regex_match(longest_match: re.Match):
-        command_method_name = longest_match.lastgroup
-        if command_method_name is not None:
-            arg_strings = [
-                value
-                for value in longest_match.groups()
-                if value is not None
-                and value != longest_match.groupdict()[command_method_name]
-            ]
-            return arg_strings
-
     async def _wrap_messages_as_async_iterable(self, messages: List[AnyStr]):
         for message in messages:
             yield message
@@ -63,47 +46,16 @@ class MultiCommandInterpreter(Interpreter[AnyStr]):
         )
         return args
 
-    def _remove_match_from_message(self, message, regex_match):
-        group_to_remove = regex_match.lastgroup
-        cut_index = regex_match.end(group_to_remove)
-        remaining_message = message[cut_index:]
-        if self.ignore_whitespace:
-            remaining_message = remaining_message.strip()
-        return remaining_message
+    @staticmethod
+    async def unknown_command() -> AsyncIterable[bytes]:
+        """An asynchronous iterable of containing a single unknown command reply.
 
-    async def _execute_matching_command(self, regex_match, command_method):
-        arg_strings = self._get_args_from_command_regex_match(regex_match)
-        args = self._convert_args_to_method_types(arg_strings, command_method)
-        response = await command_method(*args)
-        return response
-
-    async def _parse_and_execute_commands_from_message(
-        self, command_methods: dict, regexes: List[str], message: str
-    ):
-        remaining_message = message
-        responses = []
-        interrupts = []
-        while remaining_message:
-            longest_match = self._get_longest_regex_match(regexes, remaining_message)
-            if longest_match is None:
-                return (
-                    wrap_as_async_iterable(b"Command unknown"),
-                    False,
-                )
-            else:
-                command_method = command_methods[longest_match.lastgroup]
-                response = await self._execute_matching_command(
-                    longest_match, command_method
-                )
-                interrupt = command_method.__command__.interrupt
-                responses.append(response)
-                interrupts.append(interrupt)
-                remaining_message = self._remove_match_from_message(
-                    remaining_message, longest_match
-                )
-        resp = self._wrap_messages_as_async_iterable(responses)
-        interrupt = any(interrupts)
-        return resp, interrupt
+        Returns:
+            AsyncIterable[bytes]:
+                An asynchronous iterable of containing a single unknown command reply:
+                "Request does not match any known command".
+        """
+        yield b"Request does not match any known command"
 
     async def handle(
         self, adapter: Adapter, message: AnyStr
@@ -125,26 +77,63 @@ class MultiCommandInterpreter(Interpreter[AnyStr]):
                 A tuple of the asynchronous iterable of reply messages and a flag
                 indicating whether an interrupt should be raised by the adapter.
         """
-        if isinstance(message, bytes):
-            decoded_message = message.decode()
-        else:
-            decoded_message = message
+        command_methods: List[Callable] = [
+            func for _, func in getmembers(adapter) if hasattr(func, "__command__")
+        ]
 
-        command_methods: Dict[str, Callable] = {
-            name: func
-            for name, func in getmembers(adapter)
-            if hasattr(func, "__command__")
-        }
         commands = [
-            getattr(command_methods[func], "__command__", None)
-            for func in command_methods
+            cast(Optional[Command[AnyStr]], getattr(method, "__command__", None))
+            for method in command_methods
         ]
-        regexes = [
-            f"(?P<{func}>{command.regex.decode()})"
-            for command, func in zip(commands, command_methods)
-            if command is not None
-        ]
-        resp, interrupt = await self._parse_and_execute_commands_from_message(
-            command_methods, regexes, decoded_message
-        )
+
+        remaining_message = message
+        if self.ignore_whitespace:
+            remaining_message = remaining_message.strip()
+        responses = []
+        interrupts = []
+        while remaining_message:
+
+            longest_match_length = 0
+            matched_command = None
+            matched_command_method = None
+            matched_command_args = None
+
+            for command, method in zip(commands, command_methods):
+                if command is None:
+                    continue
+                parse_result = command.parse(remaining_message)
+                if parse_result is None:
+                    continue
+                args, match_start, match_end = parse_result
+                if args is None:
+                    continue
+                if match_start != 0:
+                    continue
+                if match_end > longest_match_length:
+                    longest_match_length = match_end
+                    matched_command_method = method
+                    matched_command_args = args
+                    matched_command = command
+
+            if matched_command_method is None or matched_command is None:
+                resp = MultiCommandInterpreter.unknown_command()
+                return (
+                    resp,
+                    False,
+                )
+
+            args = self._convert_args_to_method_types(
+                matched_command_args, matched_command_method
+            )
+
+            response = await matched_command_method(*args)
+            interrupt = matched_command.interrupt
+            responses.append(response)
+            interrupts.append(interrupt)
+            remaining_message = remaining_message[longest_match_length:]
+            if self.ignore_whitespace:
+                remaining_message = remaining_message.strip()
+
+        resp = self._wrap_messages_as_async_iterable(responses)
+        interrupt = any(interrupts)
         return resp, interrupt
