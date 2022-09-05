a60c8be28aed3afa5671ded74a938c596986c259
Matthew Pritchard <matthew.pritchard@diamond.ac.uk>
2022-09-05 15:59:00 +0100

    Ignore F821 undefined name 'anext' in interpreter utils tests

--- tests/adapters/interpreters/test_utils.py
+++ tests/adapters/interpreters/test_utils.py
@@ -10,16 +10,16 @@ from tickit.adapters.interpreters.utils import (
 async def test_wrap_list_correctly():
     messages = ["Hello", "World"]
     wrapped = wrap_messages_as_async_iterable(messages)
-    assert "Hello" == await anext(wrapped)
-    assert "World" == await anext(wrapped)
+    assert "Hello" == await anext(wrapped)  # noqa: F821
+    assert "World" == await anext(wrapped)  # noqa: F821
     with pytest.raises(StopAsyncIteration):
-        await anext(wrapped)
+        await anext(wrapped)  # noqa: F821
 
 
 @pytest.mark.asyncio
 async def test_wrap_message_correctly():
     message = b"Hello World"
     wrapped = wrap_as_async_iterable(message)
-    assert b"Hello World" == await anext(wrapped)
+    assert b"Hello World" == await anext(wrapped)  # noqa: F821
     with pytest.raises(StopAsyncIteration):
-        await anext(wrapped)
+        await anext(wrapped)  # noqa: F821
