396ee3875357c8f764d9bf394ac2f395f93ddda1
Matthew Pritchard <matthew.pritchard@diamond.ac.uk>
2022-09-05 13:08:53 +0100

    #92, #93: Make RegexCommand handle AnyStr and return more info

--- tests/adapters/interpreters/command/test_command_interpreter.py
+++ tests/adapters/interpreters/command/test_command_interpreter.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Callable, Optional, Sequence
+from typing import Callable, Optional, Sequence, Tuple
 
 import pytest
 from mock import AsyncMock, MagicMock, patch
@@ -11,6 +11,8 @@ from tickit.core.adapter import Adapter
 _GET_TYPE_HINTS = (
     "tickit.adapters.interpreters.command.command_interpreter.get_type_hints"
 )
+
+MOCK_PARSE_RETURN = (("arg1", "arg2"), 1, 2, 2)
 
 
 @pytest.fixture
@@ -28,9 +30,9 @@ def TestCommand():
             setattr(func, "__command__", self)
             return func
 
-        def parse(self, data: bytes) -> Optional[Sequence[str]]:
+        def parse(self, data: bytes) -> Optional[Tuple[Sequence[str], int, int, int]]:
             if data == self.command:
-                return ("arg1", "arg2")
+                return MOCK_PARSE_RETURN
             else:
                 return None
 
@@ -51,7 +53,7 @@ async def test_command_interpreter_handle_calls_func_with_args(
             __command__=MagicMock(
                 Command,
                 interrupt=False,
-                parse=MagicMock(return_value=("arg1", "arg2")),
+                parse=MagicMock(return_value=MOCK_PARSE_RETURN),
             ),
         ),
     )
@@ -74,7 +76,7 @@ async def test_command_interpreter_handle_returns_iterable_reply(
             __command__=MagicMock(
                 Command,
                 interrupt=False,
-                parse=MagicMock(return_value=("arg1", "arg2")),
+                parse=MagicMock(return_value=MOCK_PARSE_RETURN),
             ),
             return_value=reply,
         ),
@@ -97,7 +99,7 @@ async def test_command_interpreter_handle_wraps_non_iterable_reply(
             __command__=MagicMock(
                 Command,
                 interrupt=False,
-                parse=MagicMock(return_value=("arg1", "arg2")),
+                parse=MagicMock(return_value=MOCK_PARSE_RETURN),
             ),
             return_value=reply,
         ),
@@ -125,7 +127,7 @@ async def test_command_interpreter_handle_returns_interupt(
             __command__=MagicMock(
                 Command,
                 interrupt=interrupt,
-                parse=MagicMock(return_value=("arg1", "arg2")),
+                parse=MagicMock(return_value=MOCK_PARSE_RETURN),
             ),
             return_value="DummyReply",
         ),
@@ -174,3 +176,48 @@ async def test_command_interpreter_handle_returns_message_for_no_commands(
         .__aiter__()
         .__anext__()
     )
+
+
+@patch(
+    _GET_TYPE_HINTS,
+    lambda _: {"arg1": str, "arg2": str},
+)
+@pytest.mark.asyncio
+@pytest.mark.parametrize("match_end, expected_num_awaits", [(2, 0), (3, 1)])
+async def test_command_interpreter_matches_commands_against_full_message(
+    command_interpreter: CommandInterpreter, match_end: int, expected_num_awaits: int
+):
+    test_adapter = MagicMock(
+        Adapter,
+        test_method=AsyncMock(
+            __command__=MagicMock(
+                Command,
+                interrupt=False,
+                parse=MagicMock(return_value=(("arg1", "arg2"), 1, match_end, 3)),
+            ),
+        ),
+    )
+    await command_interpreter.handle(test_adapter, b"\x01")
+    assert test_adapter.test_method.await_count == expected_num_awaits
+
+
+@patch(
+    _GET_TYPE_HINTS,
+    lambda _: {"arg1": int, "arg2": float},
+)
+@pytest.mark.asyncio
+async def test_command_interpreter_converts_args_to_types_correctly(
+    command_interpreter: CommandInterpreter,
+):
+    test_adapter = MagicMock(
+        Adapter,
+        test_method=AsyncMock(
+            __command__=MagicMock(
+                Command,
+                interrupt=False,
+                parse=MagicMock(return_value=(("1", "1.0"), 1, 3, 3)),
+            ),
+        ),
+    )
+    await command_interpreter.handle(test_adapter, b"\x01")
+    assert test_adapter.test_method.awaited_with([int(1), float(1.0)])
--- tests/adapters/interpreters/command/test_regex_command.py
+++ tests/adapters/interpreters/command/test_regex_command.py
@@ -50,4 +50,74 @@ def test_regex_command_parse_unmatched_returns_none(
 def test_regex_command_parse_match_returns_args(
     regex_command: RegexCommand, message: bytes, expected: Tuple[object]
 ):
-    assert expected == regex_command.parse(message)
+    result = regex_command.parse(message)
+    assert result is not None
+    args, _, _, _ = result
+    assert expected == args
+
+
+@pytest.mark.parametrize(
+    ["regex", "interrupt", "format", "message", "expected"],
+    [
+        (r"(Test)Message", False, None, r"TestMessage", ("Test",)),
+        (b"\\x01(.)", False, None, b"\x01\x02", (b"\x02",)),
+    ],
+)
+def test_regex_command_parse_can_take_anystr(
+    regex_command: RegexCommand, message: AnyStr, expected
+):
+    result = regex_command.parse(message)
+    assert result is not None
+    args, _, _, _ = result
+    assert expected == args
+
+
+@pytest.mark.parametrize(
+    ["regex", "interrupt", "format", "message", "expected_end_of_match"],
+    [
+        (r"Test", False, None, r"TestMessage", 4),
+        (r"Test", False, "utf-32", "TestMessage".encode("utf-32"), 20),
+    ],
+)
+def test_parse_gives_right_end(
+    regex_command: RegexCommand, message: AnyStr, expected_end_of_match
+):
+    result = regex_command.parse(message)
+    assert result is not None
+    _, _, end, _ = result
+    assert end == expected_end_of_match
+
+
+@pytest.mark.parametrize(
+    ["regex", "interrupt", "format", "message", "expected_end_of_match"],
+    [
+        (r"Test", False, None, r"\nTest\r\n", 6),
+        (r"Test", False, "utf-8", b"\nTest\r\n", 4),
+    ],
+)
+def test_parse_strips_correctly_when_formatting(
+    regex_command: RegexCommand, message: AnyStr, expected_end_of_match
+):
+    result = regex_command.parse(message)
+    assert result is not None
+    _, _, end, _ = result
+    assert end == expected_end_of_match
+
+
+@pytest.mark.parametrize(
+    ["regex", "interrupt", "format", "message", "full_match_expected"],
+    [
+        (r"Test", False, None, r"\nTest\r\n", False),
+        (r"Test", False, "utf-8", b"\nTest\r\n", True),
+        (rb"Test", False, "utf-8", b"\nTest\r\n", False),
+        (rb"Test", False, None, b"\nTest\r\n", False),
+    ],
+)
+def test_parse_gives_correct_whole_matches(
+    regex_command: RegexCommand, message: AnyStr, full_match_expected
+):
+    result = regex_command.parse(message)
+    assert result is not None
+    _, _, end, message_length = result
+    full_match = end == message_length
+    assert full_match == full_match_expected
