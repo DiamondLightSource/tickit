9c4fc120b9a71cf09a90c49cb9a6192ac6377464
MattPrit <MattPrit@users.noreply.github.com>
2022-09-07 13:05:48 +0000

    deploy: f4b3625824de8c0239ece456843c71a4db7a0a49

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1+15.gc0e90c0',
+    VERSION: '0.1.1+20.gf4b3625',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
