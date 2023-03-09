b72ec6ae675ca7c0eae5baa2364019b42c28eda2
abbiemery <abbiemery@users.noreply.github.com>
2023-03-09 14:08:14 +0000

    deploy: ef1df557b5d7343a156e3ede30dfa5370d87e5ad

--- 109_fix_pytest_warnings/_static/documentation_options.js
+++ 109_fix_pytest_warnings/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.1.dev11+gb6b5391',
+    VERSION: '0.2.1.dev12+gef1df55',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
