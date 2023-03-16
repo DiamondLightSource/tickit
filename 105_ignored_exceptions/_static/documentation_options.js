dd482e13192aae38bfd00e4043f9bea0011b9e49
abbiemery <abbiemery@users.noreply.github.com>
2023-03-16 16:09:36 +0000

    deploy: 51a9c2eb9af52b8995b7f7e2c5d7e7116e413d63

--- 105_ignored_exceptions/_static/documentation_options.js
+++ 105_ignored_exceptions/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.1.dev9+gf4bf93c',
+    VERSION: '0.2.1.dev11+g51a9c2e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
