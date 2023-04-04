6b1cd920ad51b65cb27cd3626b3f384a2fd834b2
Abigail Emery <abigail.emery@diamond.ac.uk>
2023-04-04 09:46:40 +0000

    Update typedef tests for new dataclasses

--- tests/core/test_typedefs.py
+++ tests/core/test_typedefs.py
@@ -1,7 +1,16 @@
 from dataclasses import is_dataclass
 from typing import Optional
 
-from tickit.core.typedefs import Changes, ComponentID, Input, Output, SimTime
+from tickit.core.typedefs import (
+    Changes,
+    ComponentException,
+    ComponentID,
+    Input,
+    Interrupt,
+    Output,
+    SimTime,
+    StopComponent,
+)
 
 
 def test_input_is_dataclass():
@@ -18,6 +27,10 @@ def test_input_has_time():
 
 def test_input_has_changes():
     assert Changes == Input.__annotations__["changes"]
+
+
+def test_StopComponent_is_dataclass():
+    assert is_dataclass(StopComponent)
 
 
 def test_output_is_dataclass():
@@ -38,3 +51,27 @@ def test_output_has_changes():
 
 def test_output_has_call_in():
     assert Optional[SimTime] == Output.__annotations__["call_at"]
+
+
+def test_Interrupt_is_dataclass():
+    assert is_dataclass(Interrupt)
+
+
+def test_Interrupt_has_source():
+    assert ComponentID == Interrupt.__annotations__["source"]
+
+
+def test_ComponentException_is_dataclass():
+    assert is_dataclass(ComponentException)
+
+
+def test_ComponentException_has_source():
+    assert ComponentID == ComponentException.__annotations__["source"]
+
+
+def test_ComponentException_has_error():
+    assert Exception == ComponentException.__annotations__["error"]
+
+
+def test_ComponentException_has_traceback():
+    assert str == ComponentException.__annotations__["traceback"]
