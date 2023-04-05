dde8d8bf2b0331b8c16f6d5c9da780ab6c919b87
Abigail Emery <abigail.emery@diamond.ac.uk>
2023-04-05 10:56:54 +0000

    Add explicit event loop handling for cli tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -56,3 +56,14 @@ async def tickit_task(request):
         await t
     except asyncio.CancelledError:
         pass
+
+
+@pytest.fixture
+def event_loop():
+    """Manage instance of event loop for runner test cases."""
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
