df776c853127eefaba51826d880a50a6e8162a7d
Abigail Emery <abigail.emery@diamond.ac.uk>
2023-04-12 16:13:58 +0000

    Update sink device docstrings

--- src/tickit/devices/sink.py
+++ src/tickit/devices/sink.py
@@ -19,8 +19,7 @@ class SinkDevice(Device):
     Outputs: TypedDict = TypedDict("Outputs", {})
 
     def update(self, time: SimTime, inputs: Inputs) -> DeviceUpdate[Outputs]:
-        """The update method which logs the inputs at debug level and produces no
-        outputs.
+        """The update method which logs the inputs and produces no outputs.
 
         Args:
             time (SimTime): The current simulation time (in nanoseconds).
