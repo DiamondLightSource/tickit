92d7c88f976030be30db3abae1b797aeb4d42720
joeshannon <joeshannon@users.noreply.github.com>
2023-06-28 11:14:55 +0000

    deploy: e983a14c4ec2e0d8d8453743b1873cbf2ea3e0ad

--- typos/_static/documentation_options.js
+++ typos/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3.dev1+g81a1239',
+    VERSION: '0.2.3.dev1+ge983a14',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
