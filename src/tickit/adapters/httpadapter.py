763005eca1e9062dc583c00d3983ae658e6e2eda
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-06-30 13:32:09 +0100

    Create proper stop/start mechanism for HTTP adapter and add tests

--- src/tickit/adapters/httpadapter.py
+++ src/tickit/adapters/httpadapter.py
@@ -1,13 +1,13 @@
 import asyncio
 import logging
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from inspect import getmembers
 from typing import Iterable
 
 from aiohttp import web
 from aiohttp.web_routedef import RouteDef
 
-from tickit.adapters.interpreters.endpoints.http_endpoint import HTTPEndpoint
+from tickit.adapters.interpreters.endpoints.http_endpoint import HttpEndpoint
 from tickit.core.adapter import Adapter, RaiseInterrupt
 from tickit.core.device import Device
 
@@ -15,7 +15,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 @dataclass
-class HTTPAdapter(Adapter):
+class HttpAdapter(Adapter):
     """An adapter implementation which delegates to a server and sets up endpoints.
 
     An adapter implementation which delegates the hosting of an http requests to a
@@ -25,6 +25,9 @@ class HTTPAdapter(Adapter):
     host: str = "localhost"
     port: int = 8080
 
+    _started: asyncio.Event = field(default_factory=asyncio.Event)
+    _stopped: asyncio.Event = field(default_factory=asyncio.Event)
+
     async def run_forever(
         self, device: Device, raise_interrupt: RaiseInterrupt
     ) -> None:
@@ -32,13 +35,22 @@ class HTTPAdapter(Adapter):
         await super().run_forever(device, raise_interrupt)
 
         await self._start_server()
+        await self._stopped.wait()
 
-        try:
-            await asyncio.Event().wait()
-        finally:
-            # TODO: This doesn't work yet due to asyncio's own exception handler
-            await self.app.shutdown()
-            await self.app.cleanup()
+    async def wait_until_ready(self, timeout: float = 1.0) -> None:
+        """Blocks until server is ready to receive requests.
+
+        Args:
+            timeout: Raise a TimeoutError if the server is not ready within
+                this many seconds. Defaults to 1.0
+        """
+        await asyncio.wait_for(self._started.wait(), timeout=timeout)
+
+    async def stop(self) -> None:
+        await self.site.stop()
+        await self.app.shutdown()
+        await self.app.cleanup()
+        self._stopped.set()
 
     async def _start_server(self):
         LOGGER.debug(f"Starting HTTP server... {self}")
@@ -46,8 +58,9 @@ class HTTPAdapter(Adapter):
         self.app.add_routes(list(self.endpoints()))
         runner = web.AppRunner(self.app)
         await runner.setup()
-        site = web.TCPSite(runner, host=self.host, port=self.port)
-        await site.start()
+        self.site = web.TCPSite(runner, host=self.host, port=self.port)
+        await self.site.start()
+        self._started.set()
 
     def endpoints(self) -> Iterable[RouteDef]:
         """Returns list of endpoints.
@@ -56,12 +69,12 @@ class HTTPAdapter(Adapter):
         then yields them.
 
         Returns:
-            Iterable[HTTPEndpoint]: The list of defined endpoints
+            Iterable[HttpEndpoint]: The list of defined endpoints
 
         Yields:
-            Iterator[Iterable[HTTPEndpoint]]: The iterator of the defined endpoints
+            Iterator[Iterable[HttpEndpoint]]: The iterator of the defined endpoints
         """
         for _, func in getmembers(self):
             endpoint = getattr(func, "__endpoint__", None)  # type: ignore
-            if endpoint is not None and isinstance(endpoint, HTTPEndpoint):
+            if endpoint is not None and isinstance(endpoint, HttpEndpoint):
                 yield endpoint.define(func)
--- tests/adapters/test_httpadapter.py
+++ tests/adapters/test_httpadapter.py
@@ -1,12 +1,16 @@
+import asyncio
 from typing import Iterable
+import aiohttp
 
 import pytest
+import pytest_asyncio
+import requests
 from aiohttp import web
 from mock import Mock
 from mock.mock import create_autospec, patch
 
-from tickit.adapters.httpadapter import HTTPAdapter
-from tickit.adapters.interpreters.endpoints.http_endpoint import HTTPEndpoint
+from tickit.adapters.httpadapter import HttpAdapter
+from tickit.adapters.interpreters.endpoints.http_endpoint import HttpEndpoint
 from tickit.core.device import Device
 
 ISSUE_LINK = "https://github.com/dls-controls/tickit/issues/111"
@@ -25,52 +29,122 @@ def mock_raise_interrupt():
     return Mock(raise_interrupt)
 
 
-class MockAdapter(HTTPAdapter):
+class ExampleAdapter(HttpAdapter):
     device: Device
 
-    @HTTPEndpoint.get("/mock_endpoint")
-    async def mock_endpoint(self, request: web.Request) -> web.Response:
-        return web.Response(text="test")
+    @HttpEndpoint.get("/foo")
+    async def get_foo(self, request: web.Request) -> web.Response:
+        return web.json_response({"value": "foo"})
+
+    @HttpEndpoint.put("/foo")
+    async def put_foo(self, request: web.Request) -> web.Response:
+        value = (await request.json())["value"]
+        return web.json_response({"value": value})
+
+    @HttpEndpoint.post("/foo")
+    async def post_foo(self, request: web.Request) -> web.Response:
+        value = (await request.json())["value"]
+        return web.json_response({"value": value})
+
+    @HttpEndpoint.get("/bar/{name}")
+    async def get_bar(self, request: web.Request) -> web.Response:
+        name = request.match_info["name"]
+        return web.json_response({"entity": name, "value": "bar"})
+
+    @HttpEndpoint.put("/bar/{name}")
+    async def put_bar(self, request: web.Request) -> web.Response:
+        name = request.match_info["name"]
+        value = (await request.json())["value"]
+        return web.json_response({"entity": name, "value": value})
+
+    @HttpEndpoint.post("/bar/{name}")
+    async def post_bar(self, request: web.Request) -> web.Response:
+        name = request.match_info["name"]
+        value = (await request.json())["value"]
+        return web.json_response({"entity": name, "value": value})
+
+    @HttpEndpoint.get("/baz")
+    async def get_baz(self, request: web.Request) -> web.Response:
+        return web.Response(status=403)
 
 
 @pytest.fixture
-def http_adapter() -> HTTPAdapter:
-    http_adapter = HTTPAdapter()
+def adapter() -> HttpAdapter:
+    http_adapter = ExampleAdapter()
     return http_adapter
 
 
-@pytest.mark.skip(ISSUE_LINK)
-def test_http_adapter_constructor():
-    HTTPAdapter()
-
-
-@pytest.fixture
-def patch_asyncio_event_wait() -> Iterable[Mock]:
-    with patch(
-        "tickit.core.components.device_simulation.asyncio.Event.wait", autospec=True
-    ) as mock:
-        yield mock
-
-
-@pytest.mark.asyncio
-@pytest.mark.skip(ISSUE_LINK)
-async def test_http_adapter_run_forever_method(
-    http_adapter: HTTPAdapter,
-    mock_device: Device,
-    mock_raise_interrupt: Mock,
-    patch_asyncio_event_wait: Mock,
+@pytest_asyncio.fixture
+async def adapter_url(
+    adapter: HttpAdapter, mock_device: Device, event_loop: asyncio.BaseEventLoop
 ):
-    await http_adapter.run_forever(mock_device, mock_raise_interrupt)
-    await http_adapter.shutdown()
-
-    patch_asyncio_event_wait.assert_awaited_once()
+    task = event_loop.create_task(adapter.run_forever(mock_device, lambda: None))
+    await adapter.wait_until_ready(timeout=10.0)
+    yield f"http://localhost:{adapter.port}"
+    await adapter.stop()
+    await asyncio.wait_for(task, timeout=10.0)
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip(ISSUE_LINK)
-async def test_http_adapter_endpoints():
-    adapter = MockAdapter()
+async def test_http_adapter_get(adapter_url: str):
+    url = f"{adapter_url}/foo"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            assert response.status == 200
+            assert (await response.json()) == {"value": "foo"}
 
-    resp = await list(adapter.endpoints())[0].handler(None)
 
-    assert resp.text == "test"
+@pytest.mark.asyncio
+@pytest.mark.parametrize("name", ["a", "b"])
+async def test_http_adapter_get_by_name(adapter_url: str, name: str):
+    url = f"{adapter_url}/bar/{name}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            assert response.status == 200
+            assert (await response.json()) == {"entity": name, "value": "bar"}
+
+
+@pytest.mark.asyncio
+async def test_http_adapter_error_code(adapter_url: str):
+    url = f"{adapter_url}/baz"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            assert response.status == 403
+
+
+@pytest.mark.asyncio
+async def test_http_adapter_put(adapter_url: str):
+    url = f"{adapter_url}/foo"
+    async with aiohttp.ClientSession() as session:
+        async with session.put(url, json={"value": "bar"}) as response:
+            assert response.status == 200
+            assert (await response.json()) == {"value": "bar"}
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("name", ["a", "b"])
+async def test_http_adapter_put_by_name(adapter_url: str, name: str):
+    url = f"{adapter_url}/bar/{name}"
+    async with aiohttp.ClientSession() as session:
+        async with session.put(url, json={"value": "foo"}) as response:
+            assert response.status == 200
+            assert (await response.json()) == {"entity": name, "value": "foo"}
+
+
+@pytest.mark.asyncio
+async def test_http_adapter_post(adapter_url: str):
+    url = f"{adapter_url}/foo"
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json={"value": "bar"}) as response:
+            assert response.status == 200
+            assert (await response.json()) == {"value": "bar"}
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("name", ["a", "b"])
+async def test_http_adapter_post_by_name(adapter_url: str, name: str):
+    url = f"{adapter_url}/bar/{name}"
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json={"value": "foo"}) as response:
+            assert response.status == 200
+            assert (await response.json()) == {"entity": name, "value": "foo"}
