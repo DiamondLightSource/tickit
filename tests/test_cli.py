2715ee04e3cb1fa650516cc4ce732859d4cf594c
Abigail Emery <abigail.emery@diamond.ac.uk>
2023-07-05 15:14:30 +0000

    Update cli tests for new simulation wrapper

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,13 +4,9 @@ from typing import Iterable
 import pytest
 from click.testing import CliRunner, Result
 from mock import Mock, patch
-from mock.mock import create_autospec
 
 from tickit import __version__
 from tickit.cli import main
-from tickit.core.components.component import ComponentConfig
-from tickit.core.management.schedulers.master import MasterScheduler
-from tickit.core.typedefs import ComponentID, ComponentPort, PortID
 
 
 def test_cli_version():
@@ -28,27 +24,8 @@ def patch_logging() -> Iterable[Mock]:
 
 
 @pytest.fixture
-def patch_run_all_forever() -> Iterable[Mock]:
-    with patch("tickit.cli.run_all_forever", autospec=True) as mock:
-        yield mock
-
-
-@pytest.fixture
-def patch_asyncio() -> Iterable[Mock]:
-    with patch("tickit.cli.asyncio", autospec=True) as mock:
-        yield mock
-
-
-@pytest.fixture
-def patch_read_configs() -> Iterable[Mock]:
-    with patch("tickit.cli.read_configs", autospec=True) as mock:
-        mock_config = create_autospec(ComponentConfig, instance=True)
-        mock_config.name = "fake_device"
-        mock_config.inputs = {
-            PortID("42"),
-            ComponentPort(ComponentID("foo"), PortID("24")),
-        }
-        mock.return_value = [mock_config]
+def patch_run_tickit_simulation() -> Iterable[Mock]:
+    with patch("tickit.cli.TickitSimulation", autospec=True) as mock:
         yield mock
 
 
@@ -61,30 +38,55 @@ def test_cli_set_logging_level(
     patch_logging.basicConfig.assert_called_with(level="INFO")
 
 
-def test_component_command(
-    patch_run_all_forever: Mock,
-    patch_read_configs: Mock,
+def test_components_command_for_one_component(
+    patch_run_tickit_simulation: Mock,
     event_loop,
 ):
     runner: CliRunner = CliRunner()
 
     result: Result = runner.invoke(
-        main, args=["component", "fake_device", "path/to/fake_device.yaml"]
+        main, args=["components", "fake_device", "path/to/fake_device.yaml"]
     )
 
     assert result.exit_code == 0
-    patch_run_all_forever.assert_called_once()
+    patch_run_tickit_simulation.assert_called_once_with(
+        "path/to/fake_device.yaml",
+        "kafka",
+        include_schedulers=False,
+        components_to_run=("fake_device",),
+    )
 
 
-@pytest.fixture
-def patch_master_scheduler_run_forever_method() -> Iterable[Mock]:
-    with patch.object(MasterScheduler, "run_forever", autospec=True) as mock:
-        yield mock
+def test_components_command_for_multiple_components(
+    patch_run_tickit_simulation: Mock,
+    event_loop,
+):
+    runner: CliRunner = CliRunner()
+
+    result: Result = runner.invoke(
+        main,
+        args=[
+            "components",
+            "fake_device_1",
+            "fake_device_2",
+            "path/to/fake_device.yaml",
+        ],
+    )
+
+    assert result.exit_code == 0
+    patch_run_tickit_simulation.assert_called_once_with(
+        "path/to/fake_device.yaml",
+        "kafka",
+        include_schedulers=False,
+        components_to_run=(
+            "fake_device_1",
+            "fake_device_2",
+        ),
+    )
 
 
 def test_scheduler(
-    patch_read_configs: Mock,
-    patch_master_scheduler_run_forever_method: Mock,
+    patch_run_tickit_simulation: Mock,
     event_loop,
 ):
     runner: CliRunner = CliRunner()
@@ -92,12 +94,16 @@ def test_scheduler(
     result: Result = runner.invoke(main, args=["scheduler", "path/to/fake_device.yaml"])
 
     assert result.exit_code == 0
-    patch_master_scheduler_run_forever_method.assert_awaited_once()
+
+    patch_run_tickit_simulation.assert_called_once_with(
+        "path/to/fake_device.yaml",
+        "kafka",
+        include_components=False,
+    )
 
 
 def test_all(
-    patch_read_configs: Mock,
-    patch_master_scheduler_run_forever_method: Mock,
+    patch_run_tickit_simulation: Mock,
     event_loop,
 ):
     runner: CliRunner = CliRunner()
@@ -105,4 +111,7 @@ def test_all(
     result: Result = runner.invoke(main, args=["all", "path/to/fake_device.yaml"])
 
     assert result.exit_code == 0
-    patch_master_scheduler_run_forever_method.assert_awaited_once()
+    patch_run_tickit_simulation.assert_called_once_with(
+        "path/to/fake_device.yaml",
+        "internal",
+    )
