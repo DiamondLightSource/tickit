5538428ec3ff3ba2567a4f90af34bd6094e01602
tpoliaw <tpoliaw@users.noreply.github.com>
2023-07-07 10:02:58 +0000

    deploy: 5ccb7b27d1d19c35d5ed78ed9f6c03798cd9f366

--- typing_compat_removal/_static/documentation_options.js
+++ typing_compat_removal/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3.dev5+ga6cbfc21',
+    VERSION: '0.2.3.dev5+g5ccb7b27',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
