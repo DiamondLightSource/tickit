b44d88027cae1aea7fedd69ea6504eb576377f72
RAYemelyanova <RAYemelyanova@users.noreply.github.com>
2023-07-10 10:43:30 +0000

    deploy: 6e22b8d299707149d34fd2f9eb5e9d2ddc5433c3

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3.dev4+gc8e0fbdb',
+    VERSION: '0.2.3.dev5+g6e22b8d2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
