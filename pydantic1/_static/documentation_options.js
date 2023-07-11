6055a470827643a461227a2b7f1586892516cb4b
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2023-07-11 13:20:38 +0000

    deploy: e8df035e0c5c4c68c2693d85d6b3f338b34ae3a0

--- pydantic1/_static/documentation_options.js
+++ pydantic1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3.dev14+g9c8e52dd',
+    VERSION: '0.2.3.dev15+ge8df035e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
