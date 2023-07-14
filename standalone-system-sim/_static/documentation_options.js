9b1046a7646617fcf3fa416061f610d595045c20
abbiemery <abbiemery@users.noreply.github.com>
2023-07-14 12:26:02 +0000

    deploy: d6cd04f3d9b9b04fbac05baea9069c896f066392

--- standalone-system-sim/_static/documentation_options.js
+++ standalone-system-sim/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3.dev7+gc1784097',
+    VERSION: '0.2.4.dev2+gd6cd04f3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
