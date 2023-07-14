c8eb419a321bbfa5ca7984179ca18d3b2cc5d3c9
tpoliaw <tpoliaw@users.noreply.github.com>
2023-07-14 10:06:04 +0000

    deploy: 1b3ff04668487f3c80e238101fc3b43e99f140a5

--- pyright_errors/_static/documentation_options.js
+++ pyright_errors/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3.dev14+g42934976',
+    VERSION: '0.2.3.dev15+g1b3ff046',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
