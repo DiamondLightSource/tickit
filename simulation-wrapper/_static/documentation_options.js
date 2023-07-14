809da443cda2b1e6160c72f42940766e5ad8146c
abbiemery <abbiemery@users.noreply.github.com>
2023-07-14 10:08:50 +0000

    deploy: 96b6eea6f3e44a14232c65aa48edf2e54c3a6a3f

--- simulation-wrapper/_static/documentation_options.js
+++ simulation-wrapper/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3.dev34+g94af7f28',
+    VERSION: '0.2.3.dev36+g96b6eea6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
