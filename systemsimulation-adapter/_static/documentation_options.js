9e8419f8434ce65b2ae488b0a695c4bb354c5e8f
abbiemery <abbiemery@users.noreply.github.com>
2023-07-19 13:50:26 +0000

    deploy: 875fea3d37537eb94256cdb4752fc73953bbcbbd

--- systemsimulation-adapter/_static/documentation_options.js
+++ systemsimulation-adapter/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.4.dev19+g9db8d1e8',
+    VERSION: '0.2.4.dev21+g875fea3d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
