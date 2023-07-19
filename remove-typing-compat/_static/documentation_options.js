1941c437571db5e28d795c32674712acbd93cddc
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2023-07-19 10:10:31 +0000

    deploy: 01eb17b67fd46f96d77a4e774cf5ca3aad322690

--- remove-typing-compat/_static/documentation_options.js
+++ remove-typing-compat/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.4.dev6+gd6b04e25',
+    VERSION: '0.2.4.dev6+g01eb17b6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
