9ceffb784cb52beb57ae60b6f2639c49b4ff7958
abbiemery <abbiemery@users.noreply.github.com>
2023-07-25 13:45:11 +0000

    deploy: 539d44d1e4b289db85f0e12126155376b3ad7cf9

--- fix-hanging-http-tests/_static/documentation_options.js
+++ fix-hanging-http-tests/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.1.dev4+gacb6db24',
+    VERSION: '0.3.1.dev8+g539d44d1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
