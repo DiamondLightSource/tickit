bad65d533636546fd571152a94dd03d5a0ce3da2
abbiemery <abbiemery@users.noreply.github.com>
2023-07-25 12:33:29 +0000

    deploy: 96ea00828df1226b881f11b9673ba6bdca860a46

--- unpin-click/_static/documentation_options.js
+++ unpin-click/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.1.dev5+g28c3f0e8',
+    VERSION: '0.3.1.dev6+g96ea0082',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
