046495ee2ba580cd356b7625301212477f9749d6
Abigail Emery <abigail.emery@diamond.ac.uk>
2023-07-28 10:56:25 +0000

    Apply specific imports from interpreters

--- src/tickit/adapters/interpreters/command/__init__.py
+++ src/tickit/adapters/interpreters/command/__init__.py
@@ -1,4 +1,4 @@
-from tickit.adapters.interpreters.command.command_interpreter import CommandInterpreter
-from tickit.adapters.interpreters.command.regex_command import RegexCommand
+from .command_interpreter import CommandInterpreter
+from .regex_command import RegexCommand
 
 __all__ = ["CommandInterpreter", "RegexCommand"]
---
+++ src/tickit/adapters/interpreters/endpoints/__init__.py
@@ -0,0 +1,4 @@
+from .endpoint_interpreter import HttpEndpointInterpreter
+from .http_endpoint import HttpEndpoint
+
+__all__ = ["HttpEndpoint", "HttpEndpointInterpreter"]
