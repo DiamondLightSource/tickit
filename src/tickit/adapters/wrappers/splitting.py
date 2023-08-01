f1b1f527c361bd336154409528a2480342a4214d
Abigail Emery <abigail.emery@diamond.ac.uk>
2023-08-01 09:55:23 +0000

    Rename and refactor splitting wrapper

--- src/tickit/adapters/wrappers/splitting.py
+++ src/tickit/adapters/wrappers/splitting.py
@@ -1,59 +1,61 @@
 from re import Pattern, compile
-from typing import AnyStr, AsyncIterable, List, Tuple, AsyncIterator
+from typing import AnyStr, AsyncIterator, List, Tuple
 
+from tickit.adapters.tcp import CommandAdapter
 from tickit.adapters.utils import wrap_messages_as_async_iterator
-from tickit.core.adapter import Adapter, Interpreter
+from tickit.core.adapter import Interpreter
 
 
-class SplittingInterpreter(Interpreter[AnyStr]):
-    """A wrapper for an interpreter that splits a single message into multiple.
+class SplittingWrapper(Interpreter[AnyStr]):
+    """A wrapper for the CommandAdapter that splits a single message into multiple.
 
-    An interpreter wrapper class that takes a message, splits it according to a given
+    An CommandAdapter wrapper class that takes a message, splits it according to a given
     delimiter, and passes on the resulting sub-messages individually on to the
-    wrapped interpreter.
+    wrapped CommandAdapter.
     """
 
     def __init__(
         self,
-        interpreter: Interpreter[AnyStr],
+        adapter: CommandAdapter[AnyStr],
         message_delimiter: AnyStr,
     ) -> None:
-        """An interpreter decorator that splits a message into multiple sub-messages.
+        """An CommandAdapter decorator that splits a message into multiple sub-messages.
 
         Args:
-            interpreter (Interpreter): The interpreter messages are passed on to.
+            adapter (Interpreter): The CommandAdapter messages are passed on to.
             message_delimiter (AnyStr): The delimiter by which the message is split up.
                 Can be a regex pattern. Must be of the same type as the message.
         """
         super().__init__()
-        self.interpreter: Interpreter[AnyStr] = interpreter
+        self.adapter: CommandAdapter[AnyStr] = adapter
         self.delimeter: Pattern[AnyStr] = compile(message_delimiter)
 
     async def _handle_individual_messages(
         self, individual_messages: List[AnyStr]
-    ) -> List[Tuple[AsyncIterable[AnyStr], bool]]:
+    ) -> List[Tuple[AsyncIterator[AnyStr], bool]]:
         results = [
-            await self.interpreter.handle(message) for message in individual_messages
+            await self.adapter.handle(message) for message in individual_messages
         ]
         return results
 
     async def _collect_responses(
-        self, results: List[Tuple[AsyncIterable[AnyStr], bool]]
-    ) -> Tuple[AsyncIterable[AnyStr], bool]:
+        self, results: List[Tuple[AsyncIterator[AnyStr], bool]]
+    ) -> Tuple[AsyncIterator[AnyStr], bool]:
         """Combines results from handling multiple messages.
 
-        Takes the responses from when the wrapped interpreter handles multiple messages
-        and returns an appropriate composite response and interrupt. The response is
-        an asynchronous iterable of each of the individual responses, the composite
-        interrupt is a logical inclusive 'or' of all of the individual interrupts.
+        Takes the responses from when the wrapped CommandAdapter handles multiple
+        messages and returns an appropriate composite response and interrupt. The
+        response is an asynchronous iterator of each of the individual responses,
+        the composite interrupt is a logical inclusive 'or' of all of the individual
+        interrupts.
 
         Args:
-            results (List[Tuple[AsyncIterable[AnyStr], bool]]): a list of returned
+            results (List[Tuple[AsyncIterator[AnyStr], bool]]): a list of returned
                 values from the wrapped class' handle() method.
 
         Returns:
-            Tuple[AsyncIterable[AnyStr], bool]:
-                A tuple of the asynchronous iterable of reply messages and a flag
+            Tuple[AsyncIterator[AnyStr], bool]:
+                A tuple of the asynchronous iterator of reply messages and a flag
                 indicating whether an interrupt should be raised by the adapter.
         """
         individual_responses, individual_interrupts = zip(*results)
@@ -69,19 +71,19 @@ class SplittingInterpreter(Interpreter[AnyStr]):
         interrupt = any(individual_interrupts)
         return resp, interrupt
 
-    async def handle(self, message: AnyStr) -> Tuple[AsyncIterable[AnyStr], bool]:
-        """Splits a message and passes the resulting sub-messages on to an interpreter.
+    async def handle(self, message: AnyStr) -> Tuple[AsyncIterator[AnyStr], bool]:
+        """Splits a message and passes the resulting sub-messages to a CommandAdapter.
 
         Splits a given message and passes the resulting sub-messages on to an
-        interpreter. The responses to the individual sub-messages are then returned.
+        CommandAdapter. The responses to the individual sub-messages are then returned.
 
         Args:
             adapter (Adapter): The adapter in which the function should be executed
             message: (AnyStr): The message to be split up and handled.
 
         Returns:
-            Tuple[AsyncIterable[Union[str, bytes]], bool]:
-                A tuple of the asynchronous iterable of reply messages and a flag
+            Tuple[AsyncIterator[Union[str, bytes]], bool]:
+                A tuple of the asynchronous iterator of reply messages and a flag
                 indicating whether an interrupt should be raised by the adapter.
         """
         individual_messages = [
@@ -93,3 +95,6 @@ class SplittingInterpreter(Interpreter[AnyStr]):
         results = await self._handle_individual_messages(individual_messages)
 
         return await self._collect_responses(results)
+
+    def after_update(self) -> None:
+        ...
