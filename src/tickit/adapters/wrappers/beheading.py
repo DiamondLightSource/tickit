882da01cea2ef0c060f5fe671836b2e2c1a9c668
Abigail Emery <abigail.emery@diamond.ac.uk>
2023-08-01 09:59:23 +0000

    Refactor

--- src/tickit/adapters/wrappers/beheading.py
+++ src/tickit/adapters/wrappers/beheading.py
@@ -1,40 +1,38 @@
-from typing import AnyStr, AsyncIterable, Tuple
+from typing import AnyStr, AsyncIterator, Tuple
 
-from tickit.core.adapter import Adapter, Interpreter
+from tickit.adapters.tcp import CommandAdapter
+from tickit.core.adapter import Interpreter
 
 
 class BeheadingWrapper(Interpreter[AnyStr]):
-    """A wrapper for an interpreter which strips a header from a message.
+    """A wrapper for CommandAdapter which strips a header from a message.
 
-    An interpreter wrapper that takes a message, strips off a header of a fixed size
-    and passes the stripped message on to the wrapped interpreter
+    An CommandAdapter wrapper that takes a message, strips off a header of a fixed size
+    and passes the stripped message on to the wrapped CommandAdapter
     """
 
-    def __init__(self, interpreter: Interpreter[AnyStr], header_size: int) -> None:
-        """A wrapper for an interpreter which strips a header from a message.
+    def __init__(self, adapter: CommandAdapter[AnyStr], header_size: int) -> None:
+        """A wrapper for an CommandAdapter which strips a header from a message.
 
         Args:
-            interpreter (Interpreter): The interpreter the message is passed on to
+            adapter (CommandAdapter): The adapter the message is passed on to
                 after the header is stripped.
             header_size (int): The number of characters in the header.
         """
         super().__init__()
-        self.interpreter: Interpreter[AnyStr] = interpreter
+        self.adapter: CommandAdapter[AnyStr] = adapter
         self.header_size: int = header_size
 
-    async def handle(
-        self, adapter: Adapter, message: AnyStr
-    ) -> Tuple[AsyncIterable[AnyStr], bool]:
-        """Removes a header from a message and passes the message on to an interpreter.
+    async def handle(self, message: AnyStr) -> Tuple[AsyncIterator[AnyStr], bool]:
+        """Removes a header from a message and passes the message on to an adapter.
 
         Args:
-            adapter (Adapter): The adapter in which the function should be executed
             message: (AnyStr): The handled message, of which the header is removed.
 
         Returns:
-            Tuple[AsyncIterable[Union[str, bytes]], bool]:
-                A tuple of the asynchronous iterable of reply messages and a flag
+            Tuple[AsyncIterator[Union[str, bytes]], bool]:
+                A tuple of the asynchronous iterator of reply messages and a flag
                 indicating whether an interrupt should be raised by the adapter.
         """
         message = message[self.header_size :]
-        return await self.interpreter.handle(adapter, message)
+        return await self.adapter.handle(message)
