511722a797136c403b737ee14bd6757aeeb13b61
Abigail Emery <abigail.emery@diamond.ac.uk>
2023-08-02 13:24:24 +0000

    Remove all wrappers

--- src/tickit/adapters/wrappers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .beheading import BeheadingWrapper
-from .joining import JoiningWrapper
-from .splitting import SplittingWrapper
-
-__all__ = ["SplittingWrapper", "BeheadingWrapper", "JoiningWrapper"]
