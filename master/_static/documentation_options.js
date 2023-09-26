62c977f030eed31065c9d9cb4177405df97ddf55
abbiemery <abbiemery@users.noreply.github.com>
2023-09-26 12:59:34 +0000

    deploy: 36f6a163f6a2e9fa47d4e672ffe0c903f2243e7f

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.3.dev6+g4842db26',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.4.3.dev7+g36f6a163',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
