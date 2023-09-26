ad6f50de55fc551735c5e9de7ca034b8241beeb5
abbiemery <abbiemery@users.noreply.github.com>
2023-09-26 12:46:11 +0000

    deploy: aaa0c61ed9b3c0bb540b15ab2a312d0cfb426883

--- rename-scheduler/_static/documentation_options.js
+++ rename-scheduler/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.3.dev3+g4ee6bb88',
+    VERSION: '0.4.3.dev7+gaaa0c61e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
