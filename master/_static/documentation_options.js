9c99a839e23788ac09957532f59103db53517b1a
abbiemery <abbiemery@users.noreply.github.com>
2023-09-26 12:53:37 +0000

    deploy: 4842db2606c0b24ac5d9805024fb5ccfeefba6e3

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.3.dev5+g43a9930e',
+    VERSION: '0.4.3.dev6+g4842db26',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
