9cc6aa5589b7c7b2cd6a1df64dbc7633e373425e
coretl <coretl@users.noreply.github.com>
2026-01-09 10:41:46 +0000

    deploy: d2b1682c44630fa3baf8ed612adc50e17f5931e1

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.4.3.dev10+gc213d58d',
+    VERSION: '0.4.4.dev1+gd2b1682c4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -7,7 +7,7 @@ const DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: true,
+    NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
